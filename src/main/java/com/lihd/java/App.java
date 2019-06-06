a5ff0bb4d3f3979a37d5a0439a3c0b18dc31daf5
lihd <lierbai2199@outlook.com>
2019-06-06 22:37:28 +0800

    'commit'

--- src/main/java/com/lihd/java/App.java
+++ src/main/java/com/lihd/java/App.java
@@ -5,34 +5,6 @@ public class App {
 
     public static void main(String[] args) {
 
-        Test6 test2 = new Test6();
-
-        try{
-            try {
-                test2.test();
-            }catch (Exception e){
-                System.out.println(e);
-                System.out.println("654321");
-            }
-        }catch (Exception e){
-            System.out.println("1234432345432");
-        }
-
-
-        System.out.println("123456789");
-
     }
-
-
-}
-
-class Test6{
-
-    protected void test(){
-
-        throw new RuntimeException("fuck");
-
-    }
-
 
 }
