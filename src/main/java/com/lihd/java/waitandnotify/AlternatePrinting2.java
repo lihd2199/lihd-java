671975d938f7759114e5bf608219e39c8c92b276
hd_li <hd_li@ctrip.com>
2020-01-15 15:47:31 +0800

    sample for wait and notify

---
+++ src/main/java/com/lihd/java/waitandnotify/AlternatePrinting2.java
@@ -0,0 +1,46 @@
+package com.lihd.java.waitandnotify;
+
+
+/**
+ * @program: lihd-java
+ * @description: three thread Alternate Printing
+ * @author: li_hd
+ * @create: 2020-01-15 15:02
+ **/
+public class AlternatePrinting2 extends Thread {
+
+    private Integer integer;
+
+    private Integer value = 0;
+
+    private AlternatePrinting2(Integer integer) {
+        this.integer = integer;
+    }
+
+    public static void main(String[] args) {
+
+
+        AlternatePrinting2 myThread1 = new AlternatePrinting2(0);
+        AlternatePrinting2 myThread2 = new AlternatePrinting2(1);
+        AlternatePrinting2 myThread3 = new AlternatePrinting2(2);
+        myThread1.start();
+        myThread2.start();
+        myThread3.start();
+    }
+
+
+    @Override
+    public void run() {
+        while (true) {
+            if (value % 3 != integer) {
+                Thread.yield();
+                continue;
+            }
+            System.out.println(integer);
+            if (value++ == 10000) {
+                break;
+            }
+        }
+    }
+
+}
