d08f61114d4e15a32082eb9cda6077be5374033c
hd_li <hd_li@ctrip.com>
2020-01-16 19:52:18 +0800

    sample for condition

--- src/main/java/com/lihd/java/concurrent/SampleForCondition.java
+++ src/main/java/com/lihd/java/concurrent/SampleForCondition.java
@@ -1,15 +1,6 @@
 package com.lihd.java.concurrent;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -18,172 +9,54 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author: li_hd
  * @create: 2019-12-23 14:37
  **/
-public class SampleForCondition {
+public class SampleForCondition extends Thread {
 
+    private static ReentrantLock lock = new ReentrantLock();
 
-    public static void main(String[] args) throws InterruptedException {
+    private Integer integer;
 
-        SharedFiFoQueue sharedQueue = new SharedFiFoQueue(10);
+    private Integer count = 0;
 
-        //Create a producer and a consumer.
-        Thread producer = new Producer(sharedQueue);
-        Thread consumer = new Consumer(sharedQueue);
+    private Condition condition;
 
-        //Start both threads.
-        producer.start();
-        consumer.start();
-
-        //Wait for both threads to terminate.
-        producer.join();
-        consumer.join();
+    private SampleForCondition(Integer integer, Condition condition) {
+        this.integer = integer;
+        this.condition = condition;
     }
 
 
-}
+    public static void main(String[] args) {
 
-class SharedFiFoQueue {
+        Condition condition = lock.newCondition();
 
+        Thread thread1 = new SampleForCondition(1, condition);
+        Thread thread2 = new SampleForCondition(2, condition);
 
-    private Object[] elems = null;
-    private int current = 0;
-    private int placeIndex = 0;
-    private int removeIndex = 0;
-
-    private final Lock lock = new ReentrantLock();
-    private final Condition isEmpty = lock.newCondition();
-    private final Condition isFull = lock.newCondition();
-
-    public SharedFiFoQueue(int capacity) {
-        this.elems = new Object[capacity];
-    }
-
-    public void add(Object elem) throws InterruptedException {
-        lock.lock();
-        while (current >= elems.length)
-            isFull.await();
-
-        elems[placeIndex] = elem;
-
-        //We need the modulo, in order to avoid going out of bounds.
-        placeIndex = (placeIndex + 1) % elems.length;
-
-        ++current;
-
-        //Notify the consumer that there is data available.
-        isEmpty.signal();
-
-        lock.unlock();
-    }
-
-    public Object remove() throws InterruptedException {
-        Object elem = null;
-
-        lock.lock();
-        while (current <= 0)
-            isEmpty.await();
-
-        elem = elems[removeIndex];
-
-        //We need the modulo, in order to avoid going out of bounds.
-        removeIndex = (removeIndex + 1) % elems.length;
-
-        --current;
-
-        //Notify the producer that there is space available.
-        isFull.signal();
-
-        lock.unlock();
-
-        return elem;
-    }
+        thread1.start();
+        thread2.start();
 
 
-}
-
-
-class Consumer extends Thread {
-    private final Set<Object> seenObjects = new HashSet<>();
-    private int total = 0;
-    private final SharedFiFoQueue queue;
-
-    public Consumer(SharedFiFoQueue queue) {
-        this.queue = queue;
     }
 
     @Override
     public void run() {
         try {
-            do {
-                Object obj = queue.remove();
-                if(obj == null)
-                    break;
-
-                if(!seenObjects.contains(obj)) {
-                    ++total;
-                    seenObjects.add(obj);
+            while (true) {
+                lock.lock();
+                condition.signal();
+                System.out.println(integer);
+                condition.await();
+                if (count++ == 100) {
+                    return;
                 }
-
-                System.out.println("[Consumer] Read the element: " + obj.toString());
-
-            } while(true);
-        }
-        catch (InterruptedException ex) {
-            System.err.println("An InterruptedException was caught: " + ex.getMessage());
-            ex.printStackTrace();
-        }
-
-        System.out.println("\n[Consumer] " + total + " distinct words have been read...");
-    }
-}
-
-
-
-
-class Producer extends Thread {
-
-    private final static String FILENAME = "input.txt";
-
-    private final SharedFiFoQueue queue;
-
-    public Producer(SharedFiFoQueue queue) {
-        this.queue = queue;
-    }
-
-
-    @Override
-    public void run() {
-        BufferedReader rd = null;
-
-        try {
-            rd = new BufferedReader(new FileReader(new File(Objects.requireNonNull(this.getClass().getClassLoader().getResource(FILENAME)).toURI())));
-
-            String inputLine;
-            while ((inputLine = rd.readLine()) != null) {
-                String[] inputWords = inputLine.split(" ");
-
-                for (String inputWord : inputWords)
-                    queue.add(inputWord);
+                lock.unlock();
             }
 
-            //Terminate the execution.
-            queue.add(null);
-        } catch (InterruptedException ex) {
-            System.err.println("An InterruptedException was caught: " + ex.getMessage());
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            System.err.println("An IOException was caught: " + ex.getMessage());
-            ex.printStackTrace();
-        } catch (URISyntaxException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (rd != null)
-                    rd.close();
-            } catch (IOException ex) {
-                System.err.println("An IOException was caught: " + ex.getMessage());
-                ex.printStackTrace();
-            }
         }
+
 
     }
 }
+
