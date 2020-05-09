de5a95bbb3c38d6f6bbafee6ef2157e4c2469ef5
hd_li <hd_li@ctrip.com>
2020-05-09 09:49:08 +0800

    fork and join

--- src/main/java/com/lihd/java/concurrent/forkjoin/SampleForRecursiveTask.java
+++ src/main/java/com/lihd/java/concurrent/forkjoin/SampleForRecursiveTask.java
@@ -1,6 +1,10 @@
 package com.lihd.java.concurrent.forkjoin;
 
-import java.util.concurrent.RecursiveTask;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveAction;
+import java.util.logging.Logger;
 
 /**
  * @program: lihd-java
@@ -8,14 +12,44 @@ import java.util.concurrent.RecursiveTask;
  * @author: li_hd
  * @create: 2020-04-24 10:56
  **/
-public class SampleForRecursiveTask extends RecursiveTask<Integer> {
+public class SampleForRecursiveTask extends RecursiveAction {
 
 
-    @Override
-    protected Integer compute() {
-        return null;
+    private String workload = "";
+    private static final int THRESHOLD = 4;
+
+    private static Logger logger =
+            Logger.getAnonymousLogger();
+
+    public SampleForRecursiveTask(String workload) {
+        this.workload = workload;
     }
 
+    @Override
+    protected void compute() {
+        if (workload.length() > THRESHOLD) {
+            ForkJoinTask.invokeAll(createSubtasks());
+        } else {
+            processing(workload);
+        }
+    }
 
+    private List<SampleForRecursiveTask> createSubtasks() {
+        List<SampleForRecursiveTask> subtasks = new ArrayList<>();
+
+        String partOne = workload.substring(0, workload.length() / 2);
+        String partTwo = workload.substring(workload.length() / 2, workload.length());
+
+        subtasks.add(new SampleForRecursiveTask(partOne));
+        subtasks.add(new SampleForRecursiveTask(partTwo));
+
+        return subtasks;
+    }
+
+    private void processing(String work) {
+        String result = work.toUpperCase();
+        logger.info("This result - (" + result + ") - was processed by "
+                + Thread.currentThread().getName());
+    }
 
 }
