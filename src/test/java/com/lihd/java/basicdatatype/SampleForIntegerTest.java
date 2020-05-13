3d0ffadec0e8523a6deaee3d78b26d4af70cc1d2
hd_li <hd_li@ctrip.com>
2020-05-13 17:51:08 +0800

    Java : SampleForInteger

---
+++ src/test/java/com/lihd/java/basicdatatype/SampleForIntegerTest.java
@@ -0,0 +1,25 @@
+package com.lihd.java.basicdatatype;
+
+import org.junit.Test;
+
+/**
+ * @program: lihd-java
+ * @description:  Test SampleForInteger
+ * @author: li_hd
+ * @create: 2020-05-13 17:17
+ **/
+public class SampleForIntegerTest {
+
+    final SampleForInteger sampleForInteger = new SampleForInteger();
+    @Test
+    public void test_sampleForIntegerCache(){
+        sampleForInteger.sampleForIntegerCache();
+    }
+
+    @Test
+    public void test_sampleForHexString(){
+        sampleForInteger.sampleForHexString(16);
+    }
+
+
+}
