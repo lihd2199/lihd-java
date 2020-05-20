77d430d59e7f5f48c724907ed583db0df5f962d4
hd_li <hd_li@ctrip.com>
2020-05-20 20:02:12 +0800

    Spring : Sample For ApplicationContextAware

--- src/main/java/com/lihd/java/spring/aware/SampleForApplicationContextAware.java
+++ src/main/java/com/lihd/java/spring/aware/SampleForApplicationContextAware.java
@@ -1,16 +1,10 @@
 package com.lihd.java.spring.aware;
 
-import com.lihd.java.annotation.MyAnnotation;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @program: lihd-java
@@ -24,19 +18,21 @@ public class SampleForApplicationContextAware implements ApplicationContextAware
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 
-        final Map<String, Object> beansWithAnnotation = applicationContext.getBeansWithAnnotation(MyAnnotation.class);
+//        final Map<String, Object> beansWithAnnotation = applicationContext.getBeansWithAnnotation(MyAnnotation.class);
 
-        final String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+//        final String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
 
 //        Stream.of(beanDefinitionNames).collect(Collectors.toList()).forEach(System.out::println);
 
-        final MyAnnotation myAnnotation1 = applicationContext.findAnnotationOnBean("sampleForCycle1", MyAnnotation.class);
+//        final MyAnnotation myAnnotation1 = applicationContext.findAnnotationOnBean("sampleForCycle1", MyAnnotation.class);
 
-        System.out.println(getClass().getSimpleName() + " " + myAnnotation1.test());
+//        if (myAnnotation1 != null) {
+//            System.out.println(getClass().getSimpleName() + " " + myAnnotation1.test());
+//        }
 
-        for (String next : beansWithAnnotation.keySet()) {
-            System.out.println(String.format("key : %s  ; value : %s ", next, beansWithAnnotation.get(next)));
-        }
+//        for (String next : beansWithAnnotation.keySet()) {
+//            System.out.println(String.format("key : %s  ; value : %s ", next, beansWithAnnotation.get(next)));
+//        }
 
 
     }
