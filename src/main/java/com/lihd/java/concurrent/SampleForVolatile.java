fff60854c8ff8bd6ca67efba206e91f9447566ed
hd_li <hd_li@ctrip.com>
2020-07-17 10:47:28 +0800

    java ：SampleForVolatile

--- src/main/java/com/lihd/java/concurrent/SampleForVolatile.java
+++ src/main/java/com/lihd/java/concurrent/SampleForVolatile.java
@@ -7,9 +7,9 @@ package com.lihd.java.concurrent;
 public class SampleForVolatile {
 
     //测试一
-    private static Boolean isOk = true;
+//    private static Boolean isOk = true;
     //测试二
-//    private static volatile Boolean isOk = true;
+    private static volatile Boolean isOk = true;
 
     public static void main(String[] args) throws InterruptedException {
         Thread thread1 = new Thread(() -> {
@@ -21,12 +21,22 @@ public class SampleForVolatile {
             isOk = false;
         });
         Thread thread2 = new Thread(() -> {
-            System.out.println("开始循环了");
-            while (isOk) {
-//                加上这句话，可跳出循环
-//                System.out.println(Thread.currentThread().getName() + isOk);
+            try {
+                System.out.println("开始循环了");
+                while (isOk) {
+//                    加上三句这句话，可跳出循环
+//                    1、IO操作切换线程，导致线程缓存失效
+//                    2、synchronized 强制刷新缓存
+//                    3、当一个线程终止时，所有写入的变量都被刷新到主内存。例如:
+//                    现有线程A，B，在B线程中调用A.join()，那么在B中可以保证看到A线程产生的影响。
+//                    File file = new File("G://1.txt");
+//                    System.out.println(Thread.currentThread().getName() + isOk);
+//                    thread1.join();
+                }
+                System.out.println("跳出循环了");
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            System.out.println("跳出循环了");
         });
         thread1.start();
         thread2.start();
