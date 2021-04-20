8bd33cd869f9ef7820744a0630d0c50176fae07d
lihd <lihd2199@outlook.com>
2021-04-20 16:46:36 +0800

    SampleForCountDownLatch

---
+++ src/main/java/com/lihd/java/concurrent/countdownlatch/SampleForCountDownLatch2.java
@@ -0,0 +1,32 @@
+package com.lihd.java.concurrent.countdownlatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class SampleForCountDownLatch2 {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        CountDownLatch countDownLatch = new CountDownLatch(10);
+
+        for (int i = 0; i < 12; i++) {
+            new Thread(() -> {
+                try {
+                    Thread.sleep(1000L);
+                    System.out.println(Thread.currentThread().getName() + "释放");
+                    countDownLatch.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+
+        }
+
+        countDownLatch.await();
+        countDownLatch.await();
+        System.out.println(Thread.currentThread().getName() + "完成");
+
+    }
+
+
+}
