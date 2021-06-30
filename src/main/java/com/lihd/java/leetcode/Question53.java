a1b5fdf9c6575f1631b52e7ad435079dd3c4f391
lihongda <lihongda@xueqiu.com>
2021-06-30 21:45:02 +0800

    53

--- src/main/java/com/lihd/java/leetcode/Question53.java
+++ src/main/java/com/lihd/java/leetcode/Question53.java
@@ -5,21 +5,19 @@ public class Question53 {
 
     public static void main(String[] args) {
 
-
-        System.out.println(helper(new int[]{1, 2, 3, 4, -5, -6}));
-
-
+        System.out.println(helper(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
     }
 
 
     public static int helper(int[] nums) {
 
-        int pre = 0, maxAns = nums[0];
-        for (int x : nums) {
-            pre = Math.max(pre + x, x);
-            maxAns = Math.max(maxAns, pre);
+        int sum = 0;
+        int max = nums[0];
+        for (int num : nums) {
+            sum = Math.max(sum + num, num);
+            max = Math.max(max, sum);
         }
-        return maxAns;
+        return max;
     }
 
 }
